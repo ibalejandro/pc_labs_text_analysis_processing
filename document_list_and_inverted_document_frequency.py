c210a2d1372b2a21ddf882e3e7c0e7403d01caef
ibalejandro <asanch41@eafit.edu.co>
2017-09-14 16:04:09 -0500

    Adding the files to calculate the norm_term_frequency and document_list_and_inverted_document_frequency using the documents in the dataset.

---
+++ document_list_and_inverted_document_frequency.py
@@ -0,0 +1,86 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+import os
+import math
+
+WORD_RE = re.compile(r"[\w']+")
+TOTAL_NUMB_OF_DOCUMENTS = 3  # This value needs to be updated with the real total number of documents.
+
+
+# For each word, calculates which documents contain it and its Inverted Document Frequency using the given documents.
+class MRDocumentListAndInvertedDocumentFrequency(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_occurrence_for_word_and_doc_name,
+                   combiner=self.combiner_sum_occurrences_for_word_and_doc_name,
+                   reducer=self.reducer_sort_doc_names_and_get_idf_for_word),
+            MRStep(reducer=self.reducer_sort_doc_names_and_idf_for_word)
+        ]
+
+    # Yields [(word, document name), occurrence] for each word in the line.
+    def mapper_get_occurrence_for_word_and_doc_name(self, _, line):
+        # Gets the input file name.
+        try:
+            doc_name = os.getenv('mapreduce_map_input_file')
+        except KeyError:
+            doc_name = os.getenv('map_input_file')
+
+        # In order to yield a pair, the word has to match the RegularExpression WORD_RE.
+        for word in WORD_RE.findall(line):
+            yield (word.lower(), doc_name), 1
+
+    # Yields [word, (document name, cumulative_occurrences)] for each (word, document_name) key received.
+    def combiner_sum_occurrences_for_word_and_doc_name(self, word_and_doc_name, occurrences):
+        word, doc_name = word_and_doc_name
+        yield word, (doc_name, sum(occurrences))
+
+    '''
+    Yields {None, [word, (document name list, inverted document frequency)]} for each word after sorting the document
+    name list in descendant order using the cumulative occurrences of the word as criterion.
+    '''
+
+    def reducer_sort_doc_names_and_get_idf_for_word(self, word, doc_name_and_cumulative_occurrences):
+        # Converts the doc_name_and_cumulative_occurrences (Generator) to a list of tuples.
+        doc_name_and_cumulative_occurrences_list = []
+        for doc_name, cumulative_occurrences in doc_name_and_cumulative_occurrences:
+            doc_name_and_cumulative_occurrences_list.append((doc_name, cumulative_occurrences))
+
+        # Sorts the tuple list in descendant order using the cumulative occurrences as criterion.
+        doc_name_and_cumulative_occurrences_list.sort(key=lambda x: x[1], reverse=True)
+
+        # Creates a list containing only the document names after the descendant sorting.
+        doc_name_list = []
+        for i in range(0, len(doc_name_and_cumulative_occurrences_list)):
+            # The document name is at the first position of the tuple.
+            doc_name_list.append(doc_name_and_cumulative_occurrences_list[i][0])
+
+        # The length of the doc_name_list means the number of documents in which the word appears.
+        numb_of_documents = len(doc_name_list)
+        # Calculates the Inverted Document Frequency using its formula.
+        inverted_document_frequency = 1 + math.log(TOTAL_NUMB_OF_DOCUMENTS / numb_of_documents)
+        yield None, (word, (doc_name_list, inverted_document_frequency))
+
+    '''
+    Receives every pair produced by the previous Reducer because the yielded key was None.
+    Yields [word, (document name list, inverted document frequency)] for each word after sorting the words in
+    ascendant order using the Inverted Document Frequency as criterion (i.e. the words are sorted in descendant order
+    according to the number of documents in which they appear).
+    '''
+
+    def reducer_sort_doc_names_and_idf_for_word(self, _, word_doc_name_list_and_idf):
+        # Converts the word_doc_name_list_and_idf (Generator) to a list of tuples.
+        word_doc_name_list_and_idf_list = []
+        for word, (doc_name_list, idf) in word_doc_name_list_and_idf:
+            word_doc_name_list_and_idf_list.append((word, (doc_name_list, idf)))
+
+        # Sorts the tuple list in ascendant order using the Inverted Document Frequency as criterion.
+        word_doc_name_list_and_idf_list.sort(key=lambda x: x[1][1])
+
+        # Yields every record in the correct order.
+        for word, (doc_name_list, idf) in word_doc_name_list_and_idf_list:
+            yield word, (doc_name_list, idf)
+
+
+if __name__ == '__main__':
+    MRDocumentListAndInvertedDocumentFrequency.run()
---
+++ norm_term_frequency.py
@@ -0,0 +1,62 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+import os
+
+WORD_RE = re.compile(r"[\w']+")
+
+
+# Calculates the normalized Term Frequency (TF) using the given documents.
+class MRNormTermFrequency(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_occurrence_for_doc_name_and_word,
+                   combiner=self.combiner_sum_occurrences_for_doc_name_and_word,
+                   reducer=self.reducer_sort_word_and_norm_cumulative_occurrences_for_doc_name)
+        ]
+
+    # Yields [(document name, word), occurrence] for each word in the line.
+    def mapper_get_occurrence_for_doc_name_and_word(self, _, line):
+        # Gets the input file name.
+        try:
+            doc_name = os.getenv('mapreduce_map_input_file')
+        except KeyError:
+            doc_name = os.getenv('map_input_file')
+
+        # In order to yield a pair, the word has to match the RegularExpression WORD_RE.
+        for word in WORD_RE.findall(line):
+            yield (doc_name, word.lower()), 1
+
+    # Yields [document name, (word, cumulative_occurrences)] for each (document_name, word) key received.
+    def combiner_sum_occurrences_for_doc_name_and_word(self, doc_name_and_word, occurrences):
+        doc_name, word = doc_name_and_word
+        yield doc_name, (word, sum(occurrences))
+
+    '''
+    Yields [document name, list(word, norm_cumulative_occurrences)] for each document after sorting the list in
+    descendant order using the normalized cumulative occurrences as criterion.
+    '''
+
+    def reducer_sort_word_and_norm_cumulative_occurrences_for_doc_name(self, doc_name, word_and_cumulative_occurrences):
+        # Converts the word_and_cumulative_occurrences (Generator) to a list of tuples.
+        word_and_cumulative_occurrences_list = []
+        # Counts the number of terms in the document.
+        number_of_terms = 0
+        for word, cumulative_occurrences in word_and_cumulative_occurrences:
+            word_and_cumulative_occurrences_list.append((word, cumulative_occurrences))
+            number_of_terms += cumulative_occurrences
+        for i in range(0, len(word_and_cumulative_occurrences_list)):
+            word, cumulative_occurrences = word_and_cumulative_occurrences_list[i]
+            # The normalized cumulative occurrences are the cumulative occurrences divided by the number of terms in
+            # the document.
+            norm_cumulative_occurrences = cumulative_occurrences / number_of_terms
+            # The cumulative occurrences for each word are replaced by the normalized cumulative occurrences.
+            word_and_cumulative_occurrences_list[i] = (word, norm_cumulative_occurrences)
+
+        # Sorts the tuple list in descendant order using the normalized cumulative occurrences as criterion.
+        word_and_cumulative_occurrences_list.sort(key=lambda x: x[1], reverse=True)
+        yield doc_name, word_and_cumulative_occurrences_list
+
+
+if __name__ == '__main__':
+    MRNormTermFrequency.run()
