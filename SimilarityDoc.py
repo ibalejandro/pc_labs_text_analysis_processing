372c3447e2481b04352248d4e6fb16dd93c0b018
Alejocram <alejocram@gmail.com>
2017-10-28 10:39:06 -0500

    Fixing document to test similarity and optimizating mapper to yield the similarity from doc1 to doc2 and viceversa.

--- SimilarityDoc.py
+++ SimilarityDoc.py
@@ -3,28 +3,14 @@ import redis
 
 class SimilarityDoc(MRJob):
 
-    # def mapper(self, _, line):
-    #     doc_with_similarity = []
-    #     key, value = line.decode('ISO-8859-1', 'ignore').split(';;;');
-    #     values = value.decode('ISO-8859-1', 'ignore').split(';;');
-    #     for i in xrange(0, len(values), 2):
-    #         doc_name = values[i]
-    #         similarity = values[i+1]
-    #         doc_with_similarity.append([doc_name, similarity])
-    #     yield key, doc_with_similarity
-
     def mapper(self, _, line):
-        doc_with_similarity = []
-        key, doc_name, similarity = line.decode('ISO-8859-1', 'ignore').split(';;')
-        doc_with_similarity.append([doc_name, similarity])
-        yield key, doc_with_similarity
+        doc_name_1, doc_name_2, similarity = line.decode('ISO-8859-1', 'ignore').split(';;')
+        yield doc_name_1, (doc_name_2, similarity)
+        yield doc_name_2, (doc_name_1, similarity)
 
     def reducer(self, key, values):
         for value in values:
-            for item in value:
-                print ("item", item)
-                r.hset("similarity:"+key, item[0], item[1])
-            print(len(value))
+            r.hset("similarity:" + key, value[0], value[1])
             yield key, value
 
 if __name__ == '__main__':
