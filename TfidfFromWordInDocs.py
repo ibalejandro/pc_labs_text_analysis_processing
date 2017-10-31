b5252d231c86c6a69ab01a43e22badf1c9d98937
Alejocram <alejocram@gmail.com>
2017-10-30 21:10:52 -0500

    Changing Redis client to connect with the remote server and fixing data of examples datasets

--- TfidfFromWordInDocs.py
+++ TfidfFromWordInDocs.py
@@ -15,5 +15,7 @@ class TfidfFromWordInDocs(MRJob):
             yield key, value
 
 if __name__ == '__main__':
-    r = redis.StrictRedis(host='localhost', port=6379, db=7)
+    r = redis.from_url(
+        "redis://h:padd1089bb3eef4b1bf8c5cd5019461d8f7ad76b4c6960640f882ce0f2a9c86a6@ec2-34-224-49-43.compute-1.amazonaws.com:65139",
+        db=1)
     TfidfFromWordInDocs.run()
