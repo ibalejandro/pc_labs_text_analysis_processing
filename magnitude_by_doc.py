196b60a870b49c9f2730026ca952193041569962
ibalejandro <asanch41@eafit.edu.co>
2017-11-02 00:57:22 -0500

    Deleting the Redis part from he magnitude_by_doc.py file.

--- magnitude_by_doc.py
+++ magnitude_by_doc.py
@@ -17,12 +17,12 @@ class MagnitudeByDoc(MRJob):
 
     def reducer(self, key, values):
         for value in values:
-            r.set("magnitude:" + key, value)
+            # r.set("magnitude:" + key, value)
             print(key, value)
 
 
 if __name__ == '__main__':
-    r = redis.from_url(
-        "redis://h:padd1089bb3eef4b1bf8c5cd5019461d8f7ad76b4c6960640f882ce0f2a9c86a6@ec2-34-224-49-43.compute-1.amazonaws.com:65139",
-        db=1)
+    #r = redis.from_url(
+        #"redis://h:padd1089bb3eef4b1bf8c5cd5019461d8f7ad76b4c6960640f882ce0f2a9c86a6@ec2-34-224-49-43.compute-1.amazonaws.com:65139",
+        #db=1)
     MagnitudeByDoc.run()
