09572c975431bda246dadf7e0ec3358cb42e0e86
ibalejandro <asanch41@eafit.edu.co>
2017-11-02 00:24:02 -0500

    Uploading the dummy script to process MR outputs.

--- magnitude_by_doc_2.py
+++ magnitude_by_doc_2.py
@@ -4,24 +4,24 @@ from mrjob.job import MRJob
 from mrjob.step import MRStep
 
 
-class MRNormTermFrequency(MRJob):
+class MagnitudeByDoc2(MRJob):
     def steps(self):
         return [
-            MRStep(mapper=self.mapper_get_occurrence_for_doc_name_and_word,
-                   reducer=self.reducer_sum_occurrences_for_doc_name_and_word)
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer)
         ]
 
     # Yields [(document name, word), occurrence] for each word in the line.
-    def mapper_get_occurrence_for_doc_name_and_word(self, _, line):
+    def mapper(self, _, line):
         # In order to yield a pair, the word has to pass the validation filter.
-        print(line)
-        doc_name, magnitude = line.split(";;")
-        yield doc_name, magnitude
+        # print(line)
+        #doc_name, magnitude = line.split(";;")
+        yield line, 1
 
     # Yields [document name, (word, cumulative_occurrences)] for each (document_name, word) key received.
-    def reducer_sum_occurrences_for_doc_name_and_word(self, doc_name, magnitudes):
-        for magnitude in magnitudes:
-            yield doc_name, magnitude
+    def reducer(self, line, counts):
+        for count in counts:
+            print(line, count)
 
 if __name__ == '__main__':
-    MRNormTermFrequency.run()
+    MagnitudeByDoc2.run()
